790985f3d4bc661daa4fd7d8f778f5d009b603f7
Ruannino <ruanfariasdc@hotmail.com>
2022-05-31 21:23:24 -0300

    Expert TalK 31/05/22

---
+++ src/Cliente.java
@@ -0,0 +1,15 @@
+
+public class Cliente {
+	
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(string nome) {
+		this.nome = nome;
+	}
+	
+
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,9 @@
+
+public class ContaPoupanca extends Conta {
+
+	public void imprimirExtrato() {
+		System.out.println("=== Extrato Conta Poupança ====");
+		super.imprimirInfosComuns();
+}
+	
+}
